4cca854c389c46e62497e0c798f5c5edc8e72505
sherry2610 <shaharyarkhalid2610@gmail.com>
2022-06-01 05:12:05 +0500

    feat: basic hello world contract

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'artist tide path flame desk auction degree acquire frequent panda safe adult',
+  'https://rinkeby.infura.io/v3/5ec405e20f064f31a6732f8d4ec7e61f'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
